cca09339752e7e12b7d2f3a8963ad0f660fc3062
thold <thold@gmail.com>
2024-07-26 05:06:07 +0700

    fix bug refresh token

--- src/utils/axios-customize.js
+++ src/utils/axios-customize.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+import { Mutex } from "async-mutex";
 
+const mutex = new Mutex();
 
 const baseURL = import.meta.env.VITE_BACKEND_URL
 const NO_RETRY_HEADER = 'x-no-retry'
@@ -18,6 +20,9 @@ instance.defaults.headers.common = { 'Authorization': `Bearer ${localStorage.get
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
     // Do something before request is sent
+    if (typeof window !== "undefined" && window && window.localStorage && window.localStorage.getItem('access_token')) {
+        config.headers.Authorization = 'Bearer ' + window.localStorage.getItem('access_token');
+    }
     return config;
 }, function (error) {
     // Do something with request error
@@ -25,9 +30,15 @@ instance.interceptors.request.use(function (config) {
 });
 
 const handleRefreshToken = async () => {
-    const res = await instance.get('/api/v1/auth/refresh');
-    if (res && res.data) return res.data.access_token;
-    else null;
+    // const res = await instance.get('/api/v1/auth/refresh');
+    // if (res && res.data) return res.data.access_token;
+    // else null;
+    return await mutex.runExclusive(async () => {
+        const res = await instance.get('/api/v1/auth/refresh');
+        if (res && res.data) return res.data.access_token;
+        else return null;
+    });
+
 }
 
 
@@ -59,7 +70,7 @@ instance.interceptors.response.use(function (response) {
     ) {
         if (window.location.pathname !== '/'
             && !window.location.pathname.startsWith('/book')) {
-            window.location.href = '/login';
+            // window.location.href = '/login';
         }
     }
     return error?.response?.data ?? Promise.reject(error);
